8332efe8fc908b3547c65f7f24266712fc8e065b
kMatejak <ksmatejak@gmail.com>
2020-10-29 19:34:46 +0100

    toh-pt2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Sandro (Enroth)' },
+  { id: 12, name: 'Catherine' },
+  { id: 13, name: 'Haart (Enroth)' },
+  { id: 14, name: 'Roland (Enroth)' },
+  { id: 15, name: 'Christian (H3)' },
+  { id: 16, name: 'Gem (Enroth)' },
+  { id: 17, name: 'Nicolai (Enroth)' },
+];
